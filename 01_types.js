9fb77670266e210acee95768deb010c1498eb128
Volodya995 <xurshudyan71@gmail.com>
2021-01-11 16:12:03 +0400

    first commit

---
+++ 01_types.js
@@ -0,0 +1,73 @@
+// null, undefined, boolean, number, string, object, symbol
+
+
+// console.log(typeof 0)
+// console.log(typeof true)
+// console.log(typeof 'Javascript') // "", '', ``
+// console.log(typeof undefined)
+// console.log(typeof Math)
+// console.log(typeof Symbol('JS'))
+// console.log(typeof null)
+// console.log(typeof function() {})
+// console.log(typeof NaN)
+
+
+// ' ' false undefined, null, NaN 0
+
+// // '', 0, null, undefined, NaN, false
+// console.log(Boolean(''))
+// console.log(Boolean('Hello'))
+// console.log(Boolean(' '))
+// console.log(Boolean('0'))
+// console.log(Boolean(0))
+// console.log(Boolean(null))
+// console.log(Boolean([]))
+// console.log(Boolean({}))
+// console.log(Boolean(function(){}))
+
+// console.log(1 + '2')
+// console.log('' + 1 + 0)
+// console.log('' - 1 + 0)
+// console.log('3' * '8')
+// console.log(14 + 'px')
+// console.log('px5' + 10)
+// console.log('42' - 40)
+// console.log('42px' - 2)
+// console.log(null + 2)
+// console.log(undefined + 42)
+
+// console.log(NaN - 2)
+// console.log(Number(undefined))
+
+// == vs ===
+// console.log(2 == '2')
+// console.log(2 === '2')
+
+// console.log(undefined == null)
+// console.log(undefined === null)
+
+// console.log('0' == false)
+// console.log('0' == 0)
+// console.log(0 == 0)
+
+// =====
+// console.log(false == '')
+// console.log(false == [])
+// console.log(false == {})
+// console.log('' == 0)
+// console.log('' == [])
+// console.log('' == {})
+// console.log(0 == [])
+// console.log(0 == {})
+// console.log(0 == null)
+
+// console.log({} === {})
+
+// const obj = []
+// const obj2 = obj
+// const obj3 = obj2
+//
+// obj.push('js')
+// console.log(obj)
+// console.log(obj2)
+// console.log(obj3)
